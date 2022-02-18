a5fa1317ad4004c70429a07b8abbe96fa9268e8d
Samip <callmesamip@gmail.com>
2022-02-18 15:25:15 +1100

    Made few changes, the broken link issue remains unsolved

--- script.js
+++ script.js
@@ -3,19 +3,40 @@ const placeHolder= document.getElementById('holder');
 const alertBox = document.getElementById('alertBox');
 let productID;
 let str="";
+const hideAlert=()=>{
+  alertBox.classList.add('d-none');
+}
 const handleOnFetch=()=>{
   const productCode=document.querySelector('.productID').value;
-  console.log(!productCode)
   if(!productCode){
     alertBox.classList.remove('d-none');
   }else{
-    productID=productCode;
+    let generateZeros=""; //generates zeros to add at the beginnning of the product code
+    if(productCode.length<6){
+      const logProduct = 6-productCode.length;
+      console.log(logProduct);
+      for(let i =0;i<logProduct;i++){
+        generateZeros+="0"
+      }
+    }
+
+    productID=generateZeros+productCode;
     imageURL=`https://cdn0.woolworths.media/content/wowproductimages/medium/${productID}.jpg`;
     fetch(imageURL)
     .then(data=>{
-      let productUrl = data.url;
-      displayImage(productUrl,productID);
+      if(data.status===404){
+        imageURL = "./404.png"
+        displayImage(imageURL);
+      }
+      else{
+        let productUrl = data.url;
+        console.log(productUrl);
+        displayImage(productUrl,productID);
+      }
+ 
     })
+   
+    
   }
 }
 const displayImage=(url,productCode)=>{
@@ -23,6 +44,9 @@ const displayImage=(url,productCode)=>{
   const getLink = document.querySelector('#urlbar');
   if(getTheBox){
     const newUrl= `https://cdn0.woolworths.media/content/wowproductimages/medium/${productCode}.jpg`;
+    if(productCode===undefined){
+      getLink.setAttribute('src','./404.png');
+    }
     getLink.setAttribute('src',newUrl);
   }else{
     str+=` <div class="card" id="holderCard" style="width: 18rem;">
@@ -30,16 +54,15 @@ const displayImage=(url,productCode)=>{
       <i class="fa-solid fa-magnifying-glass text-success"></i>
     </h1>
     <sub class="text-center">Woolworths Image Finder</sub>
+    <p class="text-center  mt-2 d-none text-danger" id="alertBox">Please enter product id</p>
     <img src="${url}" class="card-img-top mt-5" id="urlbar" alt="...">
     <div class="card-body">
-    <input type="text" class="form-control mb-2 productID" placeholder="Enter Product ID" required>
+    <input type="text" class="form-control mb-2 productID" placeholder="Enter Product ID"  onClick = "hideAlert()" required>
     <button class="btn btn-success w-100" onClick="handleOnFetch(this)"> Find Image</button>
     </div>
   </div>`
+  productID="";
   placeHolder.innerHTML=str;
 }
 
-}
-const hideAlert=()=>{
-  alertBox.classList.add('d-none');
 }
